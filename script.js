7d578cf904b1d22312c35d6afd15cd358a10e823
Miguel Nunez <71807189+iamcodefoxx@users.noreply.github.com>
2021-07-08 08:51:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var slider = document.getElementById('slider');
 var toggle = document.getElementById('toggle');
 
 // Toggle functionality
-toggle.addEventListener('click', function() {
+toggle.addEventListener('click', () => {
     var isOpen = slider.classList.contains('slide-in');
 
     slider.setAttribute('class', isOpen ? 'slide-out' : 'slide-in');
@@ -10,13 +10,13 @@ toggle.addEventListener('click', function() {
 
 // Load day or night mode
 dayNightMode = () => {
-  const date = new Date();
-  const hour = date.getHours();
+  var date = new Date();
+  var hour = date.getHours();
 
   if(hour >= 7 && hour < 19)
-    document.body.style.background = 'linear-gradient(0.50turn, white, #66c2ff, white)';
+    document.body.style.background = 'linear-gradient(0.50turn, white, #33adff, white)';
   else
-    document.body.style.background = 'linear-gradient(0.50turn, black, #66c2ff, black)';
+    document.body.style.background = 'linear-gradient(0.50turn, black, #33adff, black)';
 }
 
 window.addEventListener('load', dayNightMode);
@@ -31,39 +31,61 @@ window.addEventListener('load', defaultWeather);
 // Event listener for click
 document.getElementById("searchWeather").addEventListener("click", () => {
   const input = document.getElementById('input');
-  loadWeather(input.value)
+  if(input.value != "")
+    loadWeather(input.value)
 });
 
 // Event listener for enter
 document.getElementById("input").addEventListener("keydown", (event) => {
   if(event.key === "Enter"){
     const input = document.getElementById('input');
-    loadWeather(input.value)
+    if(input.value != "")
+      loadWeather(input.value)
   }
 });
 
 // Request to the weather API
 loadWeather = (input) => {
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q='+input+'&units=metric&appid=16a2314e91b166c8c3c5b3c33539f22b';
-
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=16a2314e91b166c8c3c5b3c33539f22b`;
+    
   fetch(url)
-
   .then(response => {
-    if(response.ok)
-      return response.json();
-    else
-      alert('Error: ' + response.status + '\n\nFor better accuracy enter your city and full state name, for example, San Franscisco, California.\n\nIf your outside of the U.S., you may also enter your city and full country name, for example, Hong Kong, Japan');
+    if (response.ok) {
+      return response.json()
+    } else{
+      return Promise.reject(response.status)
+    } 
   })
+  .then(data => displayWeather(data))
+  .catch(error => alert('Error: ' + error + '\n\nFor better accuracy enter your city and full state name, for example, San Franscisco, California\n\nIf your in a country without states, you can enter your city and full country name, for example, Hong Kong, Japan'));
+}
+  
+displayWeather = (data) => {
+  document.getElementById("img").className = "";
+  setImage(data);
 
-  .then(data => { 
-    setImage(data);
-    document.getElementById('degrees').innerHTML = ((data.main.temp * 9/5) + 32).toFixed(0) + '&deg;<span style="font-size: 1rem;margin-top: -10px;">F</span>';
-    document.getElementById('city-name').innerHTML = data.name;
-    document.getElementById('bar').innerHTML = ' | ';
-    document.getElementById('description').innerHTML = data.weather[0].description;
-    document.getElementById("input").value = '';
-    dayNightMode();
-  });
+  document.getElementById("degrees").className = "";
+  document.getElementById("degrees").innerHTML = ((data.main.temp * 9/5) + 32).toFixed(0) + '&deg;<span style="font-size: 1rem;margin-top: -10px;">F</span>';
+  
+  document.getElementById('bar').className = "";
+  document.getElementById("bar").textContent = "|";
+
+  document.getElementById("city-name").className = "";
+  document.getElementById("city-name").textContent = data.name;
+  
+  document.getElementById('description').className = "";
+  document.getElementById("description").textContent = data.weather[0].description;
+
+  setTimeout(function() {
+    document.getElementById("img").classList.add("animatee");
+    document.getElementById("degrees").classList.add("animatee");
+    document.getElementById("bar").classList.add("animatee");
+    document.getElementById("city-name").classList.add("animatee");
+    document.getElementById("description").classList.add("animatee");
+  }, 300);
+
+  document.getElementById("input").value = '';
+  dayNightMode();
 }
 
 // Set the weather image
@@ -103,7 +125,7 @@ setImage = (data) => {
 
   if(data.weather[0].main === "Rain"){
       document.getElementById("img").src = "light_rain.svg";
-  }
-    
+  }    
 }
+
 
