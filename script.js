5592d24b06fc01d0ff7b2983227b7112ad8f2524
Miguel Nunez <71807189+iamcodefoxx@users.noreply.github.com>
2021-07-29 07:04:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-var slider = document.getElementById('slider');
-var toggle = document.getElementById('toggle');
+const slider = document.getElementById('slider');
+const toggle = document.getElementById('toggle');
 
 // Toggle functionality
 toggle.addEventListener('click', () => {
-    var isOpen = slider.classList.contains('slide-in');
-
+    let isOpen = slider.classList.contains('slide-in');
     slider.setAttribute('class', isOpen ? 'slide-out' : 'slide-in');
 });
 
 // Load day or night mode
 dayNightMode = () => {
-  var date = new Date();
-  var hour = date.getHours();
+  let date = new Date();
+  let hour = date.getHours();
 
   if(hour >= 7 && hour < 19)
     document.body.style.background = 'linear-gradient(0.50turn, white, #33adff, white)';
@@ -21,85 +20,83 @@ dayNightMode = () => {
 
 window.addEventListener('load', dayNightMode);
 
-// Load default weather
+// Event listener for searching the weather on "click" of the button
+document.getElementById("searchWeather").addEventListener("click", () => {
+  const input = document.getElementById('input');
+    loadWeather(input.value);
+});
+
+// Event listener for searching the weather on "keydown" of the enter key
+document.getElementById("input").addEventListener("keydown", (event) => {
+  if(event.key === "Enter")
+    loadWeather(input.value);
+});
+
+// Load default weather location
 defaultWeather = () => {
   loadWeather('San Francisco, California');
 };
 
 window.addEventListener('load', defaultWeather);
 
-// Event listener for click
-document.getElementById("searchWeather").addEventListener("click", () => {
-  const input = document.getElementById('input');
-  if(input.value != "")
-    loadWeather(input.value)
-});
-
-// Event listener for enter
-document.getElementById("input").addEventListener("keydown", (event) => {
-  if(event.key === "Enter"){
-    const input = document.getElementById('input');
-    if(input.value != "")
-      loadWeather(input.value)
-  }
-});
-
 // Request to the weather API
 loadWeather = (input) => {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=16a2314e91b166c8c3c5b3c33539f22b`;
     
   fetch(url)
+
   .then(response => {
-    if (response.ok) {
-      return response.json()
-    } else{
-      return Promise.reject(response.status)
-    } 
-  })
-  .then(data => displayWeather(data))
-  .catch(error => alert('Error: ' + error + '\n\nFor better accuracy enter your city and full state name, for example, San Franscisco, California\n\nIf your in a country without states, you can enter your city and full country name, for example, Hong Kong, Japan'));
+    if(!response.ok) throw Error(response.statusText);
+      return response.json();
+   })
+
+   .then(data => {
+      document.querySelector("#isValid").textContent = "";
+      displayWeather(data);
+   })
+   
+   .catch(error => document.querySelector("#isValid").textContent = error);
 }
   
 displayWeather = (data) => {
-  document.getElementById("img").className = "";
+  document.querySelector("#img").className = "";
   setImage(data);
 
-  document.getElementById("degrees").className = "";
-  document.getElementById("degrees").innerHTML = ((data.main.temp * 9/5) + 32).toFixed(0) + '&deg;<span style="font-size: 1rem;margin-top: -10px;">F</span>';
+  document.querySelector("#degrees").className = "";
+  document.querySelector("#degrees").innerHTML = ((data.main.temp * 9/5) + 32).toFixed(0) + '&deg;<span style="font-size: 1rem;margin-top: -10px;">F</span>';
   
-  document.getElementById('bar').className = "";
-  document.getElementById("bar").textContent = "|";
+  document.querySelector("#bar").className = "";
+  document.querySelector("#bar").textContent = "|";
 
-  document.getElementById("city-name").className = "";
-  document.getElementById("city-name").textContent = data.name;
+  document.querySelector("#city-name").className = "";
+  document.querySelector("#city-name").textContent = data.name;
   
-  document.getElementById('description').className = "";
-  document.getElementById("description").textContent = data.weather[0].description;
+  document.querySelector("#description").className = "";
+  document.querySelector("#description").textContent = data.weather[0].description;
 
   setTimeout(function() {
-    document.getElementById("img").classList.add("animatee");
-    document.getElementById("degrees").classList.add("animatee");
-    document.getElementById("bar").classList.add("animatee");
-    document.getElementById("city-name").classList.add("animatee");
-    document.getElementById("description").classList.add("animatee");
+    document.querySelector("#img").classList.add("animatee");
+    document.querySelector("#degrees").classList.add("animatee");
+    document.querySelector("#bar").classList.add("animatee");
+    document.querySelector("#city-name").classList.add("animatee");
+    document.querySelector("#description").classList.add("animatee");
   }, 300);
 
-  document.getElementById("input").value = '';
-  dayNightMode();
+  document.querySelector("#input").value = '';
 }
 
 // Set the weather image
 setImage = (data) => {
   if(data.weather[0].main === "Clear"){
-    document.getElementById("img").src = "sun.svg";
+    document.querySelector("#img").src = "sun.svg";
   }
 
   if(data.weather[0].main === "Snow"){
-    document.getElementById("img").src = "snow.svg";
+    document.querySelector("#img").src = "snow.svg";
   }
 
   if(data.weather[0].main === "Thunderstorm"){
-    document.getElementById("img").src = "thunderstorm.svg";
+    document.querySelector("#img").src = "thunderstorm.svg";
   }
 
   if(data.weather[0].main === "Drizzle" || 
@@ -113,19 +110,17 @@ setImage = (data) => {
     data.weather[0].main === "Ash" ||
     data.weather[0].main === "Squall" ||
     data.weather[0].main === "Tornado"){
-      document.getElementById("img").src = "drizzle.svg";
+      document.querySelector("#img").src = "drizzle.svg";
   }
 
   if(data.weather[0].main === "Clouds"){
     if(data.weather[0].description === "few clouds")
-      document.getElementById("img").src = "few_clouds.svg";
+      document.querySelector("#img").src = "few_clouds.svg";
     else
-      document.getElementById("img").src = "overcast_clouds.svg";
+      document.querySelector("#img").src = "overcast_clouds.svg";
   }
 
   if(data.weather[0].main === "Rain"){
-      document.getElementById("img").src = "light_rain.svg";
+      querySelector("#img").src = "light_rain.svg";
   }    
 }
-
-
